79b2b37fe070db7ccf28829250742ddf3eb3a667
Tsumugu Yamaguchi <tsumugu1515@gmail.com>
2023-03-04 00:57:53 +0900

    first commit

---
+++ app/src/main/java/xyz/tsumugu2626/app/la23/final2/ViewPagerAdapter.kt
@@ -0,0 +1,21 @@
+package xyz.tsumugu2626.app.la23.final2
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(mainActivity: MainActivity): FragmentStateAdapter(mainActivity) {
+
+    private val realNumPage = 3
+
+    override fun getItemCount(): Int = realNumPage + 2
+    fun getRealCount() = realNumPage
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0,3 -> TimelineDayFragment() // ThirdFragment()
+            1,4 -> TimelineDayFragment() // FirstFragment()
+            2 -> TimelineDayFragment() // SecondFragment()
+            else -> TimelineDayFragment() // ThirdFragment()
+        }
+    }
+}
